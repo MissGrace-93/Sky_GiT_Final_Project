67123ad93cac605b10aeddea7b682d815861ed8c
“Faidarus” <fatima_aidarus@hotmail.co.uk>
2021-04-25 13:39:30 +0100

    Updated Index page layout and footer

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from flaskblog import app
 
 # true if you run the script directly
-if __name__ == '__main__':
-    app.run(debug=True, port='3306')
-
 # if __name__ == '__main__':
-#     app.run(debug=True, port='8080')
+#     app.run(debug=True, port='3306')
+
+if __name__ == '__main__':
+    app.run(debug=True, port='8080')
