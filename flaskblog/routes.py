a9097bb5250d8b742c75ffeda2b8ea2615a8a8dc
Grace <gfleeming1@gmail.com>
2021-04-25 16:55:07 +0100

    most popular links added to homepage

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -284,11 +284,12 @@ def like_action(post_id, action):
 
 # Route for most likes
 @app.route('/most_liked', methods=['GET', 'POST'])
-def most_popular():
+def most_liked():
     posts = Post.query.order_by(Post.likes.desc()).limit(1).all()
     title = "Most liked blog post!"
 
     return render_template('most_popular.html', posts=posts, title=title)
+
 
 # Route for most commented
 @app.route('/most_commented', methods=['GET', 'POST'])
